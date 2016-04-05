9c20873cd102212cc83705b821653a1596ac4dfa
홍관형 <pcaqueen@bsscamp.com>
2016-04-05 19:06:12 +0900

    퇴근 직전 마지막 커밋

---
+++ Assets/Scripts/Managers/SaveDataManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class SaveDataManager : MonoBehaviour
+{
+    private static SaveDataManager instance;
+
+    public static SaveDataManager getInstance;
+    {
+        get
+        {
+          if (instance == null)
+           {
+           
+           }
+        return instance;
+        }
+      
+    }
+
+
+}
