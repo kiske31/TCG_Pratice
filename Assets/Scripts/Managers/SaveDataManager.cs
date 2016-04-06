4057e401867535186cfca20dcfd0eb6f1bf921f3
홍관형 <pcaqueen@bsscamp.com>
2016-04-06 18:42:22 +0900

    퇴근 전 마지막 커밋 (싱글톤 구현 작업중 커밋)

--- Assets/Scripts/Managers/SaveDataManager.cs
+++ Assets/Scripts/Managers/SaveDataManager.cs
@@ -6,18 +6,20 @@ public class SaveDataManager : MonoBehaviour
 {
     private static SaveDataManager instance;
 
-    public static SaveDataManager getInstance;
+    public static SaveDataManager getInstance
     {
         get
         {
           if (instance == null)
            {
-           
+                instance = new SaveDataManager();
            }
         return instance;
         }
-      
     }
 
-
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
 }
---
+++ Assets/Scripts/Ui/LobbyUi.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LobbyUi : MonoBehaviour
+{
+    void StartGameButton()
+    {
+
+    }
+
+    void ExitGameButton()
+    {
+
+    }
+
+    void ScoreButton()
+    {
+
+    }
+
+    void CreditButton()
+    {
+
+    }
+}
